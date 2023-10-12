12ea3ac5972741ecccedacd20e470cd464067be0
PradhumnyaBhosale <134285954+PradhumnyaBhosale@users.noreply.github.com>
2023-10-12 15:45:52 +0530

    Create AVG_WithLoop.cpp

---
+++ AVG_WithLoop.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+
+ 
+int main () 
+{
+int n1,n2,n3,n4,n5;
+double sum=0;
+double avg=0;
+    cout<<"\nEnter the first number";
+    cin>>n1;
+    cout<<"\nEnter the second number";
+    cin>>n2;
+    cout<<"\nEnter the third number";
+    cin>>n3;
+    cout<<"\nEnter the fourth number";
+    cin>>n4;
+    cout<<"\nEnter the fifth number";
+    cin>>n5;
+    sum = n1+n2+n3+n4+n5;
+    avg = sum/5;
+    
+    cout<<"\nAvg of numbers : "<<avg;
+    return 0;
+}
